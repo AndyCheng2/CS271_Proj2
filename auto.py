079e3a80bf3b23b6dcc9d3e77596d146f0aab7d6
AndyCheng2 <luqingcheng2@gmail.com>
2023-02-01 18:14:03 -0800

    add auto.py script

---
+++ auto.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+time.sleep(1)
+os.system('start cmd /K "python user.py B"')
+time.sleep(1)
+os.system('start cmd /K "python user.py D"')
+time.sleep(1)
+os.system('start cmd /K "python user.py A"')
+time.sleep(1)
+os.system('start cmd /K "python user.py C"')
+time.sleep(1)
+os.system('start cmd /K "python user.py E"')
